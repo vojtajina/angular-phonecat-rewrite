45535e6557a18e9731834e7963ecc1580d3b87d0
Vojta Jina <vojta@gemin-i.org>
2011-04-01 14:02:51 +0200

    Added some e2e tests...

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,40 +1,74 @@
-describe('my app', function() {
+describe('phonecat app', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().hash()).toBe("/view1");
+  it('should automatically redirect to catalog (/phones/) when location hash/fragment is empty', function() {
+    expect(browser().location().hash()).toBe("/phones/");
   });
 
-
-  describe('view1', function() {
+  describe('catalog', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+      browser().navigateTo('#/phones/');
     });
 
+    function numberOfPhones() {
+      return element('ng\\:include ul.phones li').count();
+    }
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('ng\\:view p:first').text()).
-        toMatch(/partial for view 1/)
+    it('should display all phones', function() {
+      expect(numberOfPhones()).toBe(32);
     });
 
+    it('should render sorting links', function() {
+      var content = element('ng\\:include').text(); 
+      expect(content).toMatch(/Sort by/);
+      expect(content).toMatch(/Newest/);
+      expect(content).toMatch(/Alphabetical/);
+    });
+
+    it('image should be link to phone detail', function() {
+      element('ng\\:include ul.phones li#phone-nexus-s a.thumb').click();
+      expect(browser().location().hash()).toBe("/phones/nexus-s");
+    });
+
+    it('title should be link to phone detail', function() {
+      element('ng\\:include ul.phones li#phone-nexus-s a.title').click();
+      expect(browser().location().hash()).toBe("/phones/nexus-s");
+    });
+
+    describe('sorting', function() {
+      it('should sort by age', function() {
+        element('#sort-age', 'Sort by newest link').click();
+        expect(element('ng\\:include ul.phones li:first').attr('id')).toEqual('phone-nexus-s');
+        expect(element('ng\\:include ul.phones li:last').attr('id')).toEqual('phone-htc-mytouch');
+      });
+
+      it('should sort by name', function() {
+        element('#sort-name', 'Sort by alphabet link').click();
+        expect(element('ng\\:include ul.phones li:first').attr('id')).toEqual('phone-dell-streak');
+        expect(element('ng\\:include ul.phones li:last').attr('id')).toEqual('phone-t-mobile-mytouch-4g');
+      });
+    });
+
+    describe('filtering', function() {
+      it('should search by name', function() {
+        input('predicate.$').enter('samsung');
+        expect(numberOfPhones()).toBe(8);
+      });
+
+      it('should filter by carrier', function() {
+        element('ul.carrier li:last a', 'Filter by Verizon link').click();
+        expect(numberOfPhones()).toBe(7);
+      });
+    });
   });
 
-
-  describe('view2', function() {
-
+  describe('detail', function() {
     beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view1 when user navigates to /view2', function() {
-      expect(element('ng\\:view p:first').text()).
-        toMatch(/partial for view 2/)
+      browser().navigateTo('#/phones/nexus-s');
     });
 
   });
